ff516b4ec27e4b5807ba83811b39cbc46b331acd
Qiu <liaoguiqiu@outlook.com>
2023-10-03 11:17:05 +0200

    basic data encoding

--- working_dir_root.py
+++ working_dir_root.py
@@ -1,6 +1,7 @@
 working_root = "/media/guiqiu/Installation/database/surgtoolloc2022_dataset/"
 
 
-Dataset_root =  working_root + "dataset/"
+Dataset_video_root =  working_root + "_release/training_data/video_clips/"
+Dataset_label_root =  working_root + "_release/training_data/"
 config_root =   working_root + "config/"
 Output_root =   working_root + "out/"
