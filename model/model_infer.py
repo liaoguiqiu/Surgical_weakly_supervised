a30b13271c96d8d7de6bc0559db5346ab338487b
Guiqiu Liao <guiqiu@pmacs.upenn.edu>
2024-01-05 13:57:25 -0500

    fully after pool lr decrease

--- model/model_infer.py
+++ model/model_infer.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn.functional as F
 
 from model.model_3dcnn_linear import _VideoCNN
-learningR = 0.00007
+learningR = 0.00001
 class _Model_infer(object):
     def __init__(self, GPU_mode =True,num_gpus=1):
         self.VideoNets = _VideoCNN()
