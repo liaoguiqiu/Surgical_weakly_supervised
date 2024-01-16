6c716659618c2d03e14c1d9e3c53e8c68b464541
liaoguiqiu <liaoguiqiu@outlook.com>
2024-01-16 16:38:57 -0500

    swtich between max and avg only for HW

--- model/model_3dcnn_linear.py
+++ model/model_3dcnn_linear.py
@@ -88,7 +88,7 @@ class _VideoCNN(nn.Module):
             Maxpool_keepC = nn.MaxPool3d((D,1,1),stride=(1,1,1))
         else:
             Maxpool_keepD = nn.AvgPool3d((1,H,W),stride=(1,1,1))
-            Maxpool_keepC = nn.AvgPool3d((D,1,1),stride=(1,1,1))
+            Maxpool_keepC = nn.MaxPool3d((D,1,1),stride=(1,1,1))
         
         slice_valid = Maxpool_keepD(input)
         final = Maxpool_keepC(slice_valid)
